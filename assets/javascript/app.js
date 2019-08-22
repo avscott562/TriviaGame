46371b0484d9bf10f03167d3e7324e604530db11
Scottie <avscott@pacbell.net>
2019-08-22 10:17:21 -0700

    added pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,11 @@
 //     $('#myInput').trigger('focus')
 //   });
 
+//declare variables
+//needed counters for time, number of correct answers, number of incorrect answers, number of unanswered
+//need array of objects for quesitons that will include multiple choice answers and correct answer
+//add more later
+
+//functions needed
+//new game, reset game, check for correct answer, timer
+//add more later
